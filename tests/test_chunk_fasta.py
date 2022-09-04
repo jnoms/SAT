9479b7dcf4767dae9d9583a6a0f4426628bfa1b2
jnoms <jasonlnomburg@gmail.com>
2022-09-04 11:06:14 -0700

    Add test for entire script of chunk_fasta

--- tests/test_chunk_fasta.py
+++ tests/test_chunk_fasta.py
@@ -1,6 +1,10 @@
-from sat.scripts.chunk_fasta import chunk_seq
+from sat.scripts.chunk_fasta import chunk_seq, chunk_fasta_main
+from sat.scripts.utils.misc import read_fasta_to_memory
 
 
+# ------------------------------------------------------------------------------------ #
+# Individual function tests
+# ------------------------------------------------------------------------------------ #
 def test_chunk_seq_not_overlapping():
     seq = "ABCDEFGHIJKLMNO"
     expected = ["ABCDEF", "GHIJKL", "MNO"]
@@ -18,3 +22,112 @@ def test_chunk_seq_overlapping_ODD():
     seq = "ABCDEFGHIJKLMNO"
     expected = ["ABCDEFG", "DEFGHIJ", "GHIJKLM", "JKLMNO"]
     assert chunk_seq(seq, max_length=7, output_overlapping=True) == expected
+
+
+# ------------------------------------------------------------------------------------ #
+# Whole-script tests
+# ------------------------------------------------------------------------------------ #
+def test_chunk_fasta_nonoverlapping(tmp_path):
+
+    # Define inputs
+    class args:
+        pass
+
+    args.in_fasta = "tests/test_data/t.fasta"
+    args.out_fasta = f"{tmp_path}/observed.fasta"
+    args.max_seq_length = 50
+    args.minimum_sequence_output_size = 20
+    args.overlapping_chunks = False
+    args.individual = False
+
+    # Run script
+    chunk_fasta_main(args)
+
+    # Check that the output is expected
+    expected = {
+        "PART0_100AA_seq": "MSSATGEGSQGARATYRAALNNEKRHDHVALTVPCCGTEAKVTALSPWFM",
+        "PART1_100AA_seq": "DGMLAYETVKEMLLKGEQLLFAPSNLSGYIKFLPGPRVYLVERLTGGTYS",
+    }
+    assert read_fasta_to_memory(args.out_fasta) == expected
+
+
+def test_chunk_fasta_nonoverlapping_two(tmp_path):
+
+    # Define inputs
+    class args:
+        pass
+
+    args.in_fasta = "tests/test_data/t.fasta"
+    args.out_fasta = f"{tmp_path}/observed.fasta"
+    args.max_seq_length = 50
+    args.minimum_sequence_output_size = 10
+    args.overlapping_chunks = False
+    args.individual = False
+
+    # Run script
+    chunk_fasta_main(args)
+
+    # Check that the output is expected
+    expected = {
+        "17AA_seq": "DPKGKFSNKLYKKLCGG",
+        "PART0_100AA_seq": "MSSATGEGSQGARATYRAALNNEKRHDHVALTVPCCGTEAKVTALSPWFM",
+        "PART1_100AA_seq": "DGMLAYETVKEMLLKGEQLLFAPSNLSGYIKFLPGPRVYLVERLTGGTYS",
+    }
+    assert read_fasta_to_memory(args.out_fasta) == expected
+
+
+def test_chunk_fasta_overlapping(tmp_path):
+
+    # Define inputs
+    class args:
+        pass
+
+    args.in_fasta = "tests/test_data/t.fasta"
+    args.out_fasta = f"{tmp_path}/observed.fasta"
+    args.max_seq_length = 50
+    args.minimum_sequence_output_size = 20
+    args.overlapping_chunks = True
+    args.individual = False
+
+    # Run script
+    chunk_fasta_main(args)
+
+    # Check that the output is expected
+    expected = {
+        "PART0_100AA_seq": "MSSATGEGSQGARATYRAALNNEKRHDHVALTVPCCGTEAKVTALSPWFM",
+        "PART1_100AA_seq": "HDHVALTVPCCGTEAKVTALSPWFMDGMLAYETVKEMLLKGEQLLFAPSN",
+        "PART2_100AA_seq": "DGMLAYETVKEMLLKGEQLLFAPSNLSGYIKFLPGPRVYLVERLTGGTYS",
+    }
+    assert read_fasta_to_memory(args.out_fasta) == expected
+
+
+def test_chunk_fasta_overlapping_individual(tmp_path):
+
+    # Define inputs
+    class args:
+        pass
+
+    args.in_fasta = "tests/test_data/t.fasta"
+    args.out_fasta = f"{tmp_path}/observed"
+    args.max_seq_length = 50
+    args.minimum_sequence_output_size = 20
+    args.overlapping_chunks = True
+    args.individual = True
+
+    # Run script
+    chunk_fasta_main(args)
+
+    # Check that the output is expected
+    one = read_fasta_to_memory(f"{tmp_path}/observed/PART0_100AA_seq.fasta")
+    two = read_fasta_to_memory(f"{tmp_path}/observed/PART1_100AA_seq.fasta")
+    three = read_fasta_to_memory(f"{tmp_path}/observed/PART2_100AA_seq.fasta")
+
+    assert one == {
+        "PART0_100AA_seq": "MSSATGEGSQGARATYRAALNNEKRHDHVALTVPCCGTEAKVTALSPWFM"
+    }
+    assert two == {
+        "PART1_100AA_seq": "HDHVALTVPCCGTEAKVTALSPWFMDGMLAYETVKEMLLKGEQLLFAPSN"
+    }
+    assert three == {
+        "PART2_100AA_seq": "DGMLAYETVKEMLLKGEQLLFAPSNLSGYIKFLPGPRVYLVERLTGGTYS"
+    }
