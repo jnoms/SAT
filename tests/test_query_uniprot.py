9f19b584b498658073b63237e6e7b65463f28adc
jnoms <jasonlnomburg@gmail.com>
2022-09-23 11:07:21 -0700

    Add a test, fix a test, and add request to the .toml

--- tests/test_query_uniprot.py
+++ tests/test_query_uniprot.py
@@ -45,7 +45,7 @@ def test_query_uniprot_main_no_cache(tmp_path):
         assert expected_uo.__eq__(observed_uo)
 
 
-def test_query_uniprot_main_full_cache(tmp_path):
+def test_query_uniprot_main_write_cache(tmp_path):
 
     # Define args
     class args:
@@ -54,7 +54,52 @@ def test_query_uniprot_main_full_cache(tmp_path):
     args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
     args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
     args.infile_col = 1
-    args.uniprot_cache = "{tmp_path}/af2_foldseek_result_small_uniprot_cache.pkl"
+    args.uniprot_cache = f"{tmp_path}/new_cache.pkl"
+
+    # Run script
+    query_uniprot_main(args)
+
+    # Parse the expected and observed into a dictionary of uniprot objects
+    expected_uniprot_object_dict = dict()
+    with open(
+        "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_lookup.tsv"
+    ) as infile:
+        for line in infile:
+            line = line.rstrip("\n").rstrip("\t").split("\t")
+            acc, geneName, fullName = line
+            uo = uniprot_object(acc)
+            uo.geneName = geneName
+            uo.fullName = fullName
+            expected_uniprot_object_dict[acc] = uo
+
+    observed_uniprot_object_dict = dict()
+    with open(args.uniprot_lookup_output) as infile:
+        for line in infile:
+            line = line.rstrip("\n").rstrip("\t").split("\t")
+            acc, geneName, fullName = line
+            uo = uniprot_object(acc)
+            uo.geneName = geneName
+            uo.fullName = fullName
+            observed_uniprot_object_dict[acc] = uo
+
+    # Make sure the values are teh same
+    for acc, expected_uo in expected_uniprot_object_dict.items():
+        observed_uo = observed_uniprot_object_dict[acc]
+        assert expected_uo.__eq__(observed_uo)
+
+
+def test_query_uniprot_main_read_cache(tmp_path):
+
+    # Define args
+    class args:
+        pass
+
+    args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
+    args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
+    args.infile_col = 1
+    args.uniprot_cache = (
+        "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_cache.pkl"
+    )
 
     # Run script
     query_uniprot_main(args)
