c02371a7c4acd5d0fea11bdb260f314664325e50
jnoms <jasonlnomburg@gmail.com>
2023-01-19 17:14:00 -0800

    Add test to make sure all dictionaries are ordered.

--- tests/test_sat.py
+++ tests/test_sat.py
@@ -1,11 +1,23 @@
 from sat import __version__
 
-# from sat import divide_by_two
-
 
 def test_version():
     assert __version__ == "0.1.0"
 
 
-# def test_divide_by_two():
-#     assert divide_by_two(4) == 2
+def test_dicts_are_ordered():
+    """
+    This is a major assumption of some functions. So, need to catch cases where folks
+    are trying to use a python version that doesn't use ordered dictionaries.
+    """
+
+    test_dict = dict()
+    test_dict[0] = "one"
+    test_dict[1] = "two"
+    test_dict[2] = "three"
+
+    key_list = list(test_dict.keys())
+
+    assert key_list[0] == 0
+    assert key_list[1] == 1
+    assert key_list[2] == 2
