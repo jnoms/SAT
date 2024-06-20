86f0f6c7ffee770b67af3f47b9846834591dbdcc
jnoms <jasonlnomburg@gmail.com>
2024-06-20 12:21:31 -0700

    removed tests that don't work on github actions because of changing databases

--- tests/test_aln_query_uniprot.py
+++ tests/test_aln_query_uniprot.py
@@ -28,84 +28,84 @@ def compare_uniprot_objects(expected_uniprot_object_dict, observed_uniprot_objec
     assert test_data_probably_old_count / total < 0.5
 
 
-def test_aln_query_uniprot_main_no_cache(tmp_path):
+# def test_aln_query_uniprot_main_no_cache(tmp_path):
 
-    # Define args
-    class args:
-        pass
+#     # Define args
+#     class args:
+#         pass
 
-    args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
-    args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
-    args.infile_col = 1
-    args.uniprot_cache = ""
+#     args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
+#     args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
+#     args.infile_col = 1
+#     args.uniprot_cache = ""
 
-    # Run script
-    aln_query_uniprot_main(args)
+#     # Run script
+#     aln_query_uniprot_main(args)
 
-    # Parse the expected and observed into a dictionary of uniprot objects
-    expected_uniprot_object_dict = dict()
-    with open(
-        "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_lookup.tsv"
-    ) as infile:
-        for line in infile:
-            line = line.rstrip("\n").rstrip("\t").split("\t")
-            acc, geneName, fullName = line
-            uo = uniprot_object(acc)
-            uo.geneName = geneName
-            uo.fullName = fullName
-            expected_uniprot_object_dict[acc] = uo
+#     # Parse the expected and observed into a dictionary of uniprot objects
+#     expected_uniprot_object_dict = dict()
+#     with open(
+#         "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_lookup.tsv"
+#     ) as infile:
+#         for line in infile:
+#             line = line.rstrip("\n").rstrip("\t").split("\t")
+#             acc, geneName, fullName = line
+#             uo = uniprot_object(acc)
+#             uo.geneName = geneName
+#             uo.fullName = fullName
+#             expected_uniprot_object_dict[acc] = uo
 
-    observed_uniprot_object_dict = dict()
-    with open(args.uniprot_lookup_output) as infile:
-        for line in infile:
-            line = line.rstrip("\n").rstrip("\t").split("\t")
-            acc, geneName, fullName = line
-            uo = uniprot_object(acc)
-            uo.geneName = geneName
-            uo.fullName = fullName
-            observed_uniprot_object_dict[acc] = uo
+#     observed_uniprot_object_dict = dict()
+#     with open(args.uniprot_lookup_output) as infile:
+#         for line in infile:
+#             line = line.rstrip("\n").rstrip("\t").split("\t")
+#             acc, geneName, fullName = line
+#             uo = uniprot_object(acc)
+#             uo.geneName = geneName
+#             uo.fullName = fullName
+#             observed_uniprot_object_dict[acc] = uo
 
-    compare_uniprot_objects(expected_uniprot_object_dict, observed_uniprot_object_dict)
+#     compare_uniprot_objects(expected_uniprot_object_dict, observed_uniprot_object_dict)
 
 
-def test_aln_query_uniprot_main_write_cache(tmp_path):
+# def test_aln_query_uniprot_main_write_cache(tmp_path):
 
-    # Define args
-    class args:
-        pass
+#     # Define args
+#     class args:
+#         pass
 
-    args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
-    args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
-    args.infile_col = 1
-    args.uniprot_cache = f"{tmp_path}/new_cache.pkl"
+#     args.infile = "tests/test_data/foldseek_related/af2_foldseek_result_small.m8"
+#     args.uniprot_lookup_output = f"{tmp_path}/result.tsv"
+#     args.infile_col = 1
+#     args.uniprot_cache = f"{tmp_path}/new_cache.pkl"
 
-    # Run script
-    aln_query_uniprot_main(args)
+#     # Run script
+#     aln_query_uniprot_main(args)
 
-    # Parse the expected and observed into a dictionary of uniprot objects
-    expected_uniprot_object_dict = dict()
-    with open(
-        "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_lookup.tsv"
-    ) as infile:
-        for line in infile:
-            line = line.rstrip("\n").rstrip("\t").split("\t")
-            acc, geneName, fullName = line
-            uo = uniprot_object(acc)
-            uo.geneName = geneName
-            uo.fullName = fullName
-            expected_uniprot_object_dict[acc] = uo
+#     # Parse the expected and observed into a dictionary of uniprot objects
+#     expected_uniprot_object_dict = dict()
+#     with open(
+#         "tests/test_data/foldseek_related/af2_foldseek_result_small_uniprot_lookup.tsv"
+#     ) as infile:
+#         for line in infile:
+#             line = line.rstrip("\n").rstrip("\t").split("\t")
+#             acc, geneName, fullName = line
+#             uo = uniprot_object(acc)
+#             uo.geneName = geneName
+#             uo.fullName = fullName
+#             expected_uniprot_object_dict[acc] = uo
 
-    observed_uniprot_object_dict = dict()
-    with open(args.uniprot_lookup_output) as infile:
-        for line in infile:
-            line = line.rstrip("\n").rstrip("\t").split("\t")
-            acc, geneName, fullName = line
-            uo = uniprot_object(acc)
-            uo.geneName = geneName
-            uo.fullName = fullName
-            observed_uniprot_object_dict[acc] = uo
+#     observed_uniprot_object_dict = dict()
+#     with open(args.uniprot_lookup_output) as infile:
+#         for line in infile:
+#             line = line.rstrip("\n").rstrip("\t").split("\t")
+#             acc, geneName, fullName = line
+#             uo = uniprot_object(acc)
+#             uo.geneName = geneName
+#             uo.fullName = fullName
+#             observed_uniprot_object_dict[acc] = uo
 
-    compare_uniprot_objects(expected_uniprot_object_dict, observed_uniprot_object_dict)
+#     compare_uniprot_objects(expected_uniprot_object_dict, observed_uniprot_object_dict)
 
 
 def test_aln_query_uniprot_main_read_cache(tmp_path):
--- tests/test_ete3_taxonomy.py
+++ tests/test_ete3_taxonomy.py
@@ -17,33 +17,33 @@ def test_name_to_taxid_trailing_numbers():
     assert taxon.taxonID == 493803
 
 
-@pytest.mark.ete3
-def test_taxon_list_to_lineage_counts_multi():
-    taxonIDs = {"2025360", "2200830", "1010"}
-    taxonomy_levels = "superkingdom,phylum,class,order,family,genus,species".split(",")
-    taxons = []
-    for taxonID in taxonIDs:
-        taxon = Taxon(taxonID, taxonomy_levels)
-        taxons.append(taxon)
+# @pytest.mark.ete3
+# def test_taxon_list_to_lineage_counts_multi():
+#     taxonIDs = {"2025360", "2200830", "1010"}
+#     taxonomy_levels = "superkingdom,phylum,class,order,family,genus,species".split(",")
+#     taxons = []
+#     for taxonID in taxonIDs:
+#         taxon = Taxon(taxonID, taxonomy_levels)
+#         taxons.append(taxon)
 
-    expected = {
-        "superkingdom": Counter({"Viruses": 2, "Bacteria": 1}),
-        "phylum": Counter({"Nucleocytoviricota": 2, "Bacteroidota": 1}),
-        "class": Counter({"Pokkesviricetes": 2, "Sphingobacteriia": 1}),
-        "order": Counter({"Chitovirales": 2, "Sphingobacteriales": 1}),
-        "family": Counter({"Poxviridae": 2, "Sphingobacteriaceae": 1}),
-        "genus": Counter(
-            {"Centapoxvirus": 1, "Sphingobacterium": 1, "Orthopoxvirus": 1}
-        ),
-        "species": Counter(
-            {"NY_014_poxvirus": 1, "Sphingobacterium_mizutaii": 1, "Akhmeta_virus": 1}
-        ),
-    }
+#     expected = {
+#         "superkingdom": Counter({"Viruses": 2, "Bacteria": 1}),
+#         "phylum": Counter({"Nucleocytoviricota": 2, "Bacteroidota": 1}),
+#         "class": Counter({"Pokkesviricetes": 2, "Sphingobacteriia": 1}),
+#         "order": Counter({"Chitovirales": 2, "Sphingobacteriales": 1}),
+#         "family": Counter({"Poxviridae": 2, "Sphingobacteriaceae": 1}),
+#         "genus": Counter(
+#             {"Centapoxvirus": 1, "Sphingobacterium": 1, "Orthopoxvirus": 1}
+#         ),
+#         "species": Counter(
+#             {"NY_014_poxvirus": 1, "Sphingobacterium_mizutaii": 1, "Akhmeta_virus": 1}
+#         ),
+#     }
 
-    observed, _ = taxon_list_to_lineage_counts(taxons, taxonomy_levels)
-    for level, c in observed.items():
-        for taxon, count in c.items():
-            assert expected[level][taxon] == count
+#     observed, _ = taxon_list_to_lineage_counts(taxons, taxonomy_levels)
+#     for level, c in observed.items():
+#         for taxon, count in c.items():
+#             assert expected[level][taxon] == count
 
 
 @pytest.mark.ete3
@@ -63,35 +63,35 @@ def test_taxonID_list_to_lineage_counts_empty():
             assert expected[level][taxon] == count
 
 
-@pytest.mark.ete3
-def test_taxonID_list_to_lineage_counts_unknown_taxa():
-    taxonIDs = {"2025360", "2200830", "1010", "324123412421412421"}
-    taxonomy_levels = "superkingdom,phylum,class,order,family,genus,species".split(",")
-    taxons = []
-    for taxonID in taxonIDs:
-        taxon = Taxon(taxonID, taxonomy_levels)
-        taxons.append(taxon)
+# @pytest.mark.ete3
+# def test_taxonID_list_to_lineage_counts_unknown_taxa():
+#     taxonIDs = {"2025360", "2200830", "1010", "324123412421412421"}
+#     taxonomy_levels = "superkingdom,phylum,class,order,family,genus,species".split(",")
+#     taxons = []
+#     for taxonID in taxonIDs:
+#         taxon = Taxon(taxonID, taxonomy_levels)
+#         taxons.append(taxon)
 
-    expected = {
-        "superkingdom": Counter({"Viruses": 2, "Bacteria": 1, "": 1}),
-        "phylum": Counter({"Nucleocytoviricota": 2, "Bacteroidota": 1, "": 1}),
-        "class": Counter({"Pokkesviricetes": 2, "Sphingobacteriia": 1, "": 1}),
-        "order": Counter({"Chitovirales": 2, "Sphingobacteriales": 1, "": 1}),
-        "family": Counter({"Poxviridae": 2, "Sphingobacteriaceae": 1, "": 1}),
-        "genus": Counter(
-            {"Centapoxvirus": 1, "Sphingobacterium": 1, "Orthopoxvirus": 1, "": 1}
-        ),
-        "species": Counter(
-            {
-                "NY_014_poxvirus": 1,
-                "Sphingobacterium_mizutaii": 1,
-                "Akhmeta_virus": 1,
-                "": 1,
-            }
-        ),
-    }
+#     expected = {
+#         "superkingdom": Counter({"Viruses": 2, "Bacteria": 1, "": 1}),
+#         "phylum": Counter({"Nucleocytoviricota": 2, "Bacteroidota": 1, "": 1}),
+#         "class": Counter({"Pokkesviricetes": 2, "Sphingobacteriia": 1, "": 1}),
+#         "order": Counter({"Chitovirales": 2, "Sphingobacteriales": 1, "": 1}),
+#         "family": Counter({"Poxviridae": 2, "Sphingobacteriaceae": 1, "": 1}),
+#         "genus": Counter(
+#             {"Centapoxvirus": 1, "Sphingobacterium": 1, "Orthopoxvirus": 1, "": 1}
+#         ),
+#         "species": Counter(
+#             {
+#                 "NY_014_poxvirus": 1,
+#                 "Sphingobacterium_mizutaii": 1,
+#                 "Akhmeta_virus": 1,
+#                 "": 1,
+#             }
+#         ),
+#     }
 
-    observed, _ = taxon_list_to_lineage_counts(taxons, taxonomy_levels)
-    for level, c in observed.items():
-        for taxon, count in c.items():
-            assert expected[level][taxon] == count
+#     observed, _ = taxon_list_to_lineage_counts(taxons, taxonomy_levels)
+#     for level, c in observed.items():
+#         for taxon, count in c.items():
+#             assert expected[level][taxon] == count
